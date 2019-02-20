9a5672fa284a3d9a2e965ff3a296d1abe47cb42c
olatunji agboola <olatunjiagboola@gmail.com>
2019-02-20 18:31:32 +0100

    add models for each resource

---
+++ API/models/meal.model.js
@@ -0,0 +1,9 @@
+class Meal {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.price = null;
+  }
+}
+
+export default Meal;
---
+++ API/models/order.model.js
@@ -0,0 +1,11 @@
+class Order {
+  construct() {
+    this.id = null;
+    this.customer_id = null;
+    this.vendor_id = null;
+    this.dateCreated = null;
+    this.meals = null;
+  }
+}
+
+export default Order;
