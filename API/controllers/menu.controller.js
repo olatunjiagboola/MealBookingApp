6975348f2d4287873c7fbd81e4e4883862142343
olatunji agboola <olatunjiagboola@gmail.com>
2019-02-22 17:12:20 +0100

    add controller for menu

---
+++ API/controllers/menu.controller.js
@@ -0,0 +1,22 @@
+import MenuService from '../services/menu.service';
+
+const MenuController = {
+  fetchAllMeals(req, res) {
+    const Menu = MenuService.fetchMenu();
+    return res.json({
+      status: 'Success',
+      data: Menu,
+    }).status(200);
+  },
+
+  addMenu(req, res) {
+    const Menu = req.body;
+    const createdMenu = MenuService.addMeal(Menu);
+    return res.json({
+      status: 'Success',
+      data: createdMenu,
+    }).status(201);
+  },
+};
+
+export default MenuController;
